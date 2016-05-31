6e471882a82cc46ff823983694f452f351466e0c
Jinhui Li <Jinhui.Li@cn.schindler.com>
2016-05-31 13:55:10 +0800

    initial

---
+++ teamlinkproject/teamlink/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell')
+
+
+var paths = {
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+
+};
+
+
+
+gulp.task('runKeystone', shell.task('node keystone.js'));
+gulp.task('watch', [
+
+  'watch:lint'
+]);
+
+gulp.task('default', ['watch', 'runKeystone']);
