f79667a7d15d04b28a5df15a8dbfa5717fe08134
Aiguo Ji <ai.guo.ji@cn.schindler.com>
2016-06-28 11:08:49 +0800

    Demo how to call web service api.

--- Teamlink.Web/public/js/app/document/documentController.js
+++ Teamlink.Web/public/js/app/document/documentController.js
@@ -1,5 +1,7 @@
 var mainApp = angular.module("teamlink_document", []);
          
 mainApp.controller('DocumentController', function($scope, DocumentService) {
-    $scope.value = DocumentService.multiply(2, 3);
+    DocumentService.getDocList().then(function(response) {
+        $scope.value = response.length;
+    });
 })
--- Teamlink.Web/public/js/app/document/documentService.js
+++ Teamlink.Web/public/js/app/document/documentService.js
@@ -1,10 +1,17 @@
 angular.module('teamlink_document_service', ['ngResource'])
 
-.factory('DocumentService', ['$resource', function() {
+.factory('DocumentService', ['$http', function($http) {
     var service = {};
+    var host= 'http://localhost:3000/';
     
     service.multiply = function(a, b) {
        return a * b
+    }
+
+    service.getDocList = function() {
+        return $http.get(host + 'api/post/list').then(function(response) {
+            return response.data;
+        });
     }
 
     return service;
