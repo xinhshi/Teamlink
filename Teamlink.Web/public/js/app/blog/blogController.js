a51e0961d68bda132b209357c57f4ca0e364451c
Xinhui Shi <Sally.Shi@cn.schindler.com>
2016-06-29 10:39:15 +0800

    category

--- Teamlink.Web/public/js/app/blog/blogController.js
+++ Teamlink.Web/public/js/app/blog/blogController.js
@@ -1,11 +1,19 @@
 angular.module('teamlink_blog', [])
 
-.controller('BlogController', ['$routeParams', '$location', '$scope', 'PostService', function($routeParams, $location, $scope, PostService) {
+.controller('BlogController', ['$routeParams', '$location', '$scope', 'PostService','PostCategory', function($routeParams, $location, $scope, PostService, PostCategory) {
  
 
  PostService.getTitle({slug: $routeParams.slug}).then(function(data) {
         $scope.blogList = data;
         $scope.value = data.length;
+
+   })
+
+ PostCategory.getCategory({slug: $routeParams.slug}).then(function(data) {
+      
+
+        $scope.categoryList = data;
+        $scope.catvalue = data.length;
 
 
    });
--- Teamlink.Web/public/js/app/post/postService.js
+++ Teamlink.Web/public/js/app/post/postService.js
@@ -6,41 +6,35 @@ angular.module('postServices', ['ngResource'])
     var service = {};
     var host= 'http://localhost:3000/';
     
-
     service.getTitle = function() {
         return $http.get(host + 'api/post/list').then(function(response) {
             return response.data;
         });
     }
-
     return service;
 }])
 
 .factory('PostCategory', ['$http', function($http){
     var service = {};
     var host= 'http://localhost:3000/';
-    
 
     service.getCategory = function() {
-        return $http.get(host + 'api/post-category/:key').then(function(response) {
+        return $http.get(host + 'api/post-category/list').then(function(response) {
             return response.data;
         });
     }
-
     return service;
 }])
 
 .factory('PostByCategory', ['$http', function($http){
     var service = {};
     var host= 'http://localhost:3000/';
-    
 
     service.getPostCategory = function() {
         return $http.get(host + 'api/post-by-category/:key').then(function(response) {
             return response.data;
         });
     }
-
     return service;
 }]);
 
